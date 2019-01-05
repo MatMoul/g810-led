f722362d664d93705abdf322393c15483f6371a4
MatMoul <matmoul@gmail.com>
2019-01-06 00:48:31 +0100

    Reverting back LICENCE file and updating src licence header file

--- src/helpers/help.h
+++ src/helpers/help.h
@@ -3,7 +3,7 @@
 
   g810-led is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License.
+  the Free Software Foundation, version 3 of the License.
 
   g810-led is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
