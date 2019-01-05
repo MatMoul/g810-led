0ca32299ac408ffe87381748de39e3a4785a8d59
MatMoul <matmoul@gmail.com>
2019-01-05 02:48:43 +0100

    Update files for licences accuracy

--- src/helpers/help.cpp
+++ src/helpers/help.cpp
@@ -1,3 +1,19 @@
+/*
+  This file is part of g810-led.
+
+  g810-led is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License.
+
+  g810-led is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with g810-led.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "help.h"
 
 #include <iostream>
